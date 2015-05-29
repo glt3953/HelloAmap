d52c5b6c7a293b48584452cc4a575deb96651ed5
glt3953 <395313544@qq.com>
2015-05-29 20:10:34 +0800

    通过实现一个继承自UIWindow的子类PasswordInputWindow，完成了密码输入界面的显示和逻辑处理

--- HelloAmap/AppDelegate.m
+++ HelloAmap/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PasswordInputWindow.h"
 
 @interface AppDelegate ()
 
@@ -36,6 +37,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[PasswordInputWindow sharedInstance] show];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
